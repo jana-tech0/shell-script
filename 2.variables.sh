59e8af03e170625b8b20325b0e83452d57026991
jana-tech0 <janu.incloud@gmail.com>
2026-05-27 16:25:20 +0530

    push

---
+++ 2.variables.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+name=$1
+age=$2
+city=$3
+profession=$4
+
+echo "Name: $name"
+echo "Age: $age"
+echo "City: $city"
+echo "Profession: $profession"
+
+username=$1
+userage=$2
+usercity=$3
+userjob=$4
+usersalary=$5
+
+echo "username: $username"
+echo "age: $userage"
+echo "city: $usercity"
+echo "salary: $usersalay"
+
+
+num1=$6
+num2=$7
+
+sum=$((num1 + num2))
+echo "The sum of $num1 and $num2 is : $sum"
--- hello-world.sh
+++ hello-world.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 echo "hello world"
 echo "iam learning the shell script"
 echo "learn start the script"
