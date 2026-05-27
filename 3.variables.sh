ce35139cad8ab1a14b00022d8d3a122fad725fc2
jana-tech0 <janu.incloud@gmail.com>
2026-05-27 16:29:36 +0530

    push

---
+++ 3.variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+read -p "Enter you name, age, city and profession: " name age city profession
+
+echo "Name: $name"
+echo "Age: $age"
+echo "City: $city"
+echo "profession: $profession"
+
+read -p "enter your username"
+read -sp "enter you password"
+
+echo ""
+echo "username: $username"
+echo "password: $password"
